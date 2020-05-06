f7a748408c13cf245d62c09e4d1084622c24ee8e
sj <sebastianthegreatful@gmail.com>
2020-05-06 21:09:17 +0700

    Introduce accuracy threshold and only select locations which are within that threshold

--- app/src/main/java/dk/fitfit/runtracker/data/db/LocationDao.kt
+++ app/src/main/java/dk/fitfit/runtracker/data/db/LocationDao.kt
@@ -5,12 +5,15 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 
+private const val ACCURACY_THRESHOLD = 50
+private const val sql = "SELECT * FROM LocationEntity WHERE runId = :runId AND accuracy < $ACCURACY_THRESHOLD ORDER BY dateTime DESC"
+
 @Dao
 interface LocationDao {
-    @Query("SELECT * FROM LocationEntity WHERE runId = :runId ORDER BY dateTime DESC")
+    @Query(sql)
     fun getLiveLocations(runId: Long): LiveData<List<LocationEntity>>
 
-    @Query("SELECT * FROM LocationEntity WHERE runId = :runId ORDER BY dateTime DESC")
+    @Query(sql)
     fun getLocations(runId: Long): List<LocationEntity>
 
     @Insert
